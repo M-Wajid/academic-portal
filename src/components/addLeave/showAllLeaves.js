c847c6b9e202c739d170fc40060a873344fecacf
M-Wajid <wajidaman5@gmail.com>
2023-03-30 15:34:07 +0500

    leave edit permission fixed

--- src/components/addLeave/showAllLeaves.js
+++ src/components/addLeave/showAllLeaves.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Table from "react-bootstrap/Table";
 import { leaveDeleted } from "../../redux/actions";
 import { useState } from "react";
-import DeleteConfirmation from './../deleteConfirmation/DeleteConfirmation';
+import DeleteConfirmation from "./../deleteConfirmation/DeleteConfirmation";
 import EditLeave from "./editLeave";
 
 const ShowAllLeaves = () => {
@@ -13,60 +13,84 @@ const ShowAllLeaves = () => {
   const [show1, setShow1] = useState(false);
   const [course, setCourse] = useState();
   const [deleteID, setDeleteID] = useState();
-  const [newLeave, setNewLeave] = useState({leaveFrom: "", LeaveTo: ""});
+  const [newLeave, setNewLeave] = useState({ leaveFrom: "", LeaveTo: "" });
   const dispatch = useDispatch();
 
-  const handleDelete = (key,id) => {
+  const handleDelete = (key, id) => {
     setCourse(key);
     setDeleteID(id);
     setShow(true);
-  }
+  };
 
-  const handleEdit = (key,item) => {
-    setNewLeave({"LeaveFrom":item.LeaveFrom, "LeaveTo":item.LeaveTo});
+  const handleEdit = (key, item) => {
+    setNewLeave({ LeaveFrom: item.LeaveFrom, LeaveTo: item.LeaveTo });
     setCourse(key);
     setDeleteID(item.id);
     setShow1(true);
-  }
+  };
   const deleteFunc = () => {
-    dispatch(leaveDeleted(course,deleteID));
-  }
+    dispatch(leaveDeleted(course, deleteID));
+  };
 
   return (
     <>
-    <EditLeave show={show1} setShow={setShow1} id={deleteID} course={course} newLeave={newLeave} setNewLeave={setNewLeave}/>
-    <DeleteConfirmation show={show} setShow={setShow} deleteFunc={deleteFunc}/>
-    <Table bordered hover>
-      <thead>
-        <th>Course</th>
-        <th>Student Name</th>
-        <th>Teacher Name</th>
-        <th>Leave From</th>
-        <th>Leave To</th>
-        <th>Teacher Approval</th>
-        <th>Admin Approval</th>
-        <th>Actions</th>
-      </thead>
-      <tbody>
-        {Object.keys(leaveState).map((key) =>
-          leaveState[key].map(
-            (item) =>
-              item.StudentName === user.name && (
-                <tr>
-                  <td>{key}</td>
-                  {Object.keys(item).map(
-                    (el) => el !== "id" && <td>{item[el]}</td>
-                  )}
-                  <td>
-                    <button className="Button" onClick={() => handleEdit(key,item)}>Edit</button>
-                    <button className="Button" onClick={() => handleDelete(key,item.id)}>Delete</button>
-                  </td>
-                </tr>
-              )
-          )
-        )}
-      </tbody>
-    </Table>
+      <EditLeave
+        show={show1}
+        setShow={setShow1}
+        id={deleteID}
+        course={course}
+        newLeave={newLeave}
+        setNewLeave={setNewLeave}
+      />
+      <DeleteConfirmation
+        show={show}
+        setShow={setShow}
+        deleteFunc={deleteFunc}
+      />
+      <Table bordered hover>
+        <thead>
+          <th>Course</th>
+          <th>Student Name</th>
+          <th>Teacher Name</th>
+          <th>Leave From</th>
+          <th>Leave To</th>
+          <th>Teacher Approval</th>
+          <th>Admin Approval</th>
+          <th>Actions</th>
+        </thead>
+        <tbody>
+          {Object.keys(leaveState).map((key) =>
+            leaveState[key].map(
+              (item) =>
+                item.StudentName === user.name && (
+                  <tr>
+                    <td>{key}</td>
+                    {Object.keys(item).map(
+                      (el) => el !== "id" && <td>{item[el]}</td>
+                    )}
+                    <td>
+                    {item.TeacherApproval === " - " ? (
+                        <button
+                          className="Button"
+                          onClick={() => handleEdit(key, item)}
+                        >
+                          Edit
+                        </button>
+                    ) : ( 
+                      <button
+                      className="Button"
+                      onClick={() => handleDelete(key, item.id)}
+                    >
+                      Delete
+                    </button>
+                    )}
+                    </td>
+                  </tr>
+                )
+            )
+          )}
+        </tbody>
+      </Table>
     </>
   );
 };
