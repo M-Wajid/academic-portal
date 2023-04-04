bc40b90011a7d396ecb4de85ce0c52a20c09a4c2
M-Wajid <wajidaman5@gmail.com>
2023-04-04 15:35:18 +0500

    student marks and attendance edited

--- src/components/ViewMarks/viewCourseMarks.js
+++ src/components/ViewMarks/viewCourseMarks.js
@@ -1,4 +1,4 @@
-import "../../styles/table-style.css"
+import "../../styles/table-style.css";
 import { useSelector } from "react-redux";
 
 const ViewCourseMarks = (props) => {
@@ -6,32 +6,35 @@ const ViewCourseMarks = (props) => {
   const marksState = useSelector((state) => state.marksReducer);
   const { course } = props;
   return (
-    <table className="styled-table">
-      <thead>
-        <tr>
-          <th>Task Type</th>
-          <th>Obtained Marks</th>
-          <th>Total Marks</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.keys(marksState).map((key) =>
-          key === course
-            ? Object.keys(marksState[key]).map((item) =>
-                marksState[key][item].map((el) =>
-                  el.Student_Name === user.name ? (
-                    <tr>
-                      <th>{item}</th>
-                      <td>{el.Obtained_Marks}</td>
-                      <td>{el.Total_Marks}</td>
-                    </tr>
-                  ) : null
+    <>
+      <h2>{course}</h2>
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th>Task Type</th>
+            <th>Obtained Marks</th>
+            <th>Total Marks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(marksState).map((key) =>
+            key === course
+              ? Object.keys(marksState[key]).map((item) =>
+                  marksState[key][item].map((el) =>
+                    el.Student_Name === user.name ? (
+                      <tr>
+                        <th>{item}</th>
+                        <td>{el.Obtained_Marks}</td>
+                        <td>{el.Total_Marks}</td>
+                      </tr>
+                    ) : null
+                  )
                 )
-              )
-            : null
-        )}
-      </tbody>
-    </table>
+              : null
+          )}
+        </tbody>
+      </table>
+    </>
   );
 };
 
--- src/components/viewAttendance/viewCourseAttendance.js
+++ src/components/viewAttendance/viewCourseAttendance.js
@@ -1,37 +1,40 @@
 import { useSelector } from "react-redux";
-import "../../styles/table-style.css"
+import "../../styles/table-style.css";
 
 const ViewCourseAttendance = (props) => {
   const attendanceState = useSelector((state) => state.attendanceReducer);
   const user = JSON.parse(localStorage.getItem("data"));
   const { course } = props;
   return (
-    <table className="styled-table">
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.keys(attendanceState).map((key) =>
-          key === course
-            ? attendanceState[key].map((item) =>
-                item.Name === user.name
-                  ? Object.keys(item).map((key) =>
-                      key === "Name" ? null : (
-                        <tr>
-                          <td>{key}</td>
-                          <td>{item[key]}</td>
-                        </tr>
+    <>
+      <h2>{course}</h2>
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(attendanceState).map((key) =>
+            key === course
+              ? attendanceState[key].map((item) =>
+                  item.Name === user.name
+                    ? Object.keys(item).map((key) =>
+                        key === "Name" ? null : (
+                          <tr>
+                            <td>{key}</td>
+                            <td>{item[key]}</td>
+                          </tr>
+                        )
                       )
-                    )
-                  : null
-              )
-            : null
-        )}
-      </tbody>
-    </table>
+                    : null
+                )
+              : null
+          )}
+        </tbody>
+      </table>
+    </>
   );
 };
 
