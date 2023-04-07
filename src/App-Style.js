eeba9b616c2abc014bf4407cf010792697c69bf2
M-Wajid <wajidaman5@gmail.com>
2023-04-07 16:09:07 +0500

    app style js file created for antd layout

---
+++ src/App-Style.js
@@ -0,0 +1,18 @@
+
+
+export const headerStyle = {
+  backgroundColor: "rgb(0, 18, 52)",
+  color: "White",
+  fontSize: "24px",
+  fontWeight: "Bold"
+}
+
+export const siderStyle = {
+  backgroundColor: "rgb(0, 18, 52)",
+  color: "White",
+  height: "100vh"
+}
+
+export const contentStyle = {
+  padding: "24px"
+}
